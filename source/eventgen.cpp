37aa02b6032fd4aeb70d9f6826322590b61e4e1a
Nicolas Pope <nwpope@gmail.com>
2013-05-22 13:12:24 +0100

    Event generator dialog

---
+++ source/eventgen.cpp
@@ -0,0 +1,252 @@
+/*
+ * eventgen.cpp
+ *
+ *  Created on: 22 May 2013
+ *      Author: nick
+
+Copyright (c) 2013, dharc ltd.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies, 
+either expressed or implied, of the FreeBSD Project.
+ */
+
+#include "eventgen.h"
+#include <qt4/QtGui/QWidget>
+#include <qt4/QtGui/QHBoxLayout>
+#include <qt4/QtGui/QVBoxLayout>
+#include <iostream>
+#include <dsb/nid.h>
+#include <dsb/errors.h>
+
+EventGenerator::EventGenerator()
+	: QWidget()
+{
+	QVBoxLayout *mainlayout = new QVBoxLayout();
+	QHBoxLayout *typelayout = new QHBoxLayout();
+	QHBoxLayout *destlayout = new QHBoxLayout();
+	QHBoxLayout *deflayout = new QHBoxLayout();
+	QHBoxLayout *deplayout = new QHBoxLayout();
+	QHBoxLayout *buttonlayout = new QHBoxLayout();
+
+	mainlayout->addLayout(typelayout);
+	mainlayout->addLayout(destlayout);
+	mainlayout->addLayout(deflayout);
+	mainlayout->addLayout(deplayout);
+	mainlayout->addLayout(buttonlayout);
+	setLayout(mainlayout);
+
+	m_typelabel = new QLabel("Type:",this);
+	typelayout->addWidget(m_typelabel);
+
+	//Type combobox
+	m_type = new QComboBox(this);
+	typelayout->addWidget(m_type);
+	m_type->addItem("DEFINE");
+	m_type->addItem("NOTIFY");
+	m_type->addItem("GET");
+	m_type->addItem("DEP");
+
+	//NIDs for Event destinations
+	m_destlabel = new QLabel("Destination:");
+	destlayout->addWidget(m_destlabel);
+	m_dest1 = new QLineEdit();
+	m_dest1->setAutoFillBackground(true);
+	destlayout->addWidget(m_dest1);
+	m_dest1vl = new QLabel();
+	QPixmap tick("./data/icons/greentick.png");
+	m_dest1vl->setPixmap(tick);
+	destlayout->addWidget(m_dest1vl);
+	m_dest2 = new QLineEdit();
+	m_dest2->setAutoFillBackground(true);
+	destlayout->addWidget(m_dest2);
+	m_dest2vl = new QLabel();
+	//QPixmap tick("./data/icons/greentick.png");
+	m_dest2vl->setPixmap(tick);
+	destlayout->addWidget(m_dest2vl);
+
+	//Definition NID
+	m_deflabel = new QLabel("Definition/Value:");
+	deflayout->addWidget(m_deflabel);
+	m_def = new QLineEdit();
+	m_def->setAutoFillBackground(true);
+	deflayout->addWidget(m_def);
+	m_defvl = new QLabel();
+	//QPixmap tick("./data/icons/greentick.png");
+	m_defvl->setPixmap(tick);
+	deflayout->addWidget(m_defvl);
+
+	m_eval = new QComboBox();
+	deflayout->addWidget(m_eval);
+	m_eval->addItem("Constant");
+	m_eval->addItem("Simple");
+	m_eval->addItem("Agent");
+
+	//Dependency NIDs
+	m_deplabel = new QLabel("Dependency:");
+	deplayout->addWidget(m_deplabel);
+	m_dep1 = new QLineEdit();
+	m_dep1->setAutoFillBackground(true);
+	m_dep1->setEnabled(false);
+	deplayout->addWidget(m_dep1);
+	m_dep1vl = new QLabel();
+	//QPixmap tick("./data/icons/greentick.png");
+	m_dep1vl->setPixmap(tick);
+	deplayout->addWidget(m_dep1vl);
+	m_dep2 = new QLineEdit();
+	m_dep2->setAutoFillBackground(true);
+	m_dep2->setEnabled(false);
+	deplayout->addWidget(m_dep2);
+	m_dep2vl = new QLabel();
+	//QPixmap tick("./data/icons/greentick.png");
+	m_dep2vl->setPixmap(tick);
+	deplayout->addWidget(m_dep2vl);
+
+	m_send = new QPushButton("Send");
+	buttonlayout->addWidget(m_send);
+	m_cancel = new QPushButton("Cancel");
+	buttonlayout->addWidget(m_cancel);
+
+	setWindowTitle("Event Generator");
+	//resize(400,400);
+	show();
+
+	connect(m_type, SIGNAL(currentIndexChanged(int)), this, SLOT(typechanged(int)));
+	connect(m_dest1, SIGNAL(textChanged(const QString &)), this, SLOT(dest1changed(const QString &)));
+	connect(m_dest2, SIGNAL(textChanged(const QString &)), this, SLOT(dest2changed(const QString &)));
+	connect(m_def, SIGNAL(textChanged(const QString &)), this, SLOT(defchanged(const QString &)));
+	connect(m_cancel, SIGNAL(clicked()), this, SLOT(cancelclicked()));
+	connect(m_send, SIGNAL(clicked()), this, SLOT(sendclicked()));
+}
+
+EventGenerator::~EventGenerator()
+{
+
+}
+
+void EventGenerator::dest1changed(const QString &txt)
+{
+	if (txt.length() == 0)
+	{
+		QPixmap tick("./data/icons/greentick.png");
+		m_dest1vl->setPixmap(tick);
+		return;
+	}
+
+	if (dsb_nid_fromStr(txt.toAscii().constData(),&(m_event.d1)) != SUCCESS)
+	{
+		QPixmap cross("./data/icons/redcross.png");
+		m_dest1vl->setPixmap(cross);
+	}
+	else
+	{
+		QPixmap tick("./data/icons/greentick.png");
+		m_dest1vl->setPixmap(tick);
+	}
+}
+
+void EventGenerator::dest2changed(const QString &txt)
+{
+	if (txt.length() == 0)
+	{
+		QPixmap tick("./data/icons/greentick.png");
+		m_dest2vl->setPixmap(tick);
+		return;
+	}
+
+	if (dsb_nid_fromStr(txt.toAscii().constData(),&(m_event.d2)) != SUCCESS)
+	{
+		QPixmap cross("./data/icons/redcross.png");
+		m_dest2vl->setPixmap(cross);
+	}
+	else
+	{
+		QPixmap tick("./data/icons/greentick.png");
+		m_dest2vl->setPixmap(tick);
+	}
+}
+
+void EventGenerator::defchanged(const QString &txt)
+{
+	if (txt.length() == 0)
+	{
+		QPixmap tick("./data/icons/greentick.png");
+		m_defvl->setPixmap(tick);
+		return;
+	}
+
+	if (dsb_nid_fromStr(txt.toAscii().constData(),&(m_event.def)) != SUCCESS)
+	{
+		QPixmap cross("./data/icons/redcross.png");
+		m_defvl->setPixmap(cross);
+	}
+	else
+	{
+		QPixmap tick("./data/icons/greentick.png");
+		m_defvl->setPixmap(tick);
+	}
+}
+
+void EventGenerator::sendclicked()
+{
+	//Actually send the event here.
+}
+
+void EventGenerator::cancelclicked()
+{
+	hide();
+}
+
+void EventGenerator::typechanged(int index)
+{
+	switch(index)
+	{
+	case 0:	m_event.type = EVENT_DEFINE;
+			m_def->setEnabled(true);
+			m_eval->setEnabled(true);
+			m_dep1->setEnabled(false);
+			m_dep2->setEnabled(false);
+			break;
+	case 1: m_event.type = EVENT_NOTIFY;
+			m_def->setEnabled(false);
+			m_eval->setEnabled(false);
+			m_dep1->setEnabled(false);
+			m_dep2->setEnabled(false);
+			break;
+	case 2: m_event.type = EVENT_GET;
+			m_def->setEnabled(false);
+			m_eval->setEnabled(false);
+			m_dep1->setEnabled(false);
+			m_dep2->setEnabled(false);
+			break;
+	case 3: m_event.type = EVENT_DEP;
+			m_def->setEnabled(false);
+			m_eval->setEnabled(false);
+			m_dep1->setEnabled(true);
+			m_dep2->setEnabled(true);
+			break;
+	default: break;
+	}
+}
+
---
+++ source/eventgen.h
@@ -0,0 +1,88 @@
+/*
+ * eventgen.h
+ *
+ *  Created on: 22 May 2013
+ *      Author: nick
+
+Copyright (c) 2013, dharc ltd.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies, 
+either expressed or implied, of the FreeBSD Project.
+ */
+
+#ifndef EVENTGEN_H_
+#define EVENTGEN_H_
+
+#include <qt4/QtGui/QWidget>
+#include <qt4/QtGui/QLineEdit>
+#include <qt4/QtGui/QComboBox>
+#include <qt4/QtGui/QPushButton>
+#include <qt4/QtGui/QLabel>
+#include <dsb/event.h>
+
+class EventGenerator : public QWidget
+{
+		Q_OBJECT
+
+public:
+		EventGenerator();
+		~EventGenerator();
+
+private:
+		QLabel *m_typelabel;
+		QLabel *m_destlabel;
+		QComboBox *m_type;
+		QLineEdit *m_dest1;
+		QLabel *m_dest1vl;
+		QLabel *m_dest2vl;
+		QLineEdit *m_dest2;
+
+		QLabel *m_deflabel;
+		QLineEdit *m_def;
+		QComboBox *m_eval;
+
+		QLabel *m_deplabel;
+		QLineEdit *m_dep1;
+		QLineEdit *m_dep2;
+		QLabel *m_defvl;
+		QLabel *m_dep1vl;
+		QLabel *m_dep2vl;
+
+		QPushButton *m_send;
+		QPushButton *m_cancel;
+
+		Event_t m_event;
+
+public slots:
+	void dest1changed(const QString &);
+	void dest2changed(const QString &);
+	void defchanged(const QString &);
+	void sendclicked();
+	void cancelclicked();
+	void typechanged(int index);
+};
+
+
+#endif /* EVENTGEN_H_ */
